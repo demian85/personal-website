ab1af6c7533c8b4491bb6706c9cb0e05867f30a9
Demian Rodriguez <demian85@gmail.com>
2017-11-18 11:05:04 -0300

    Remove unused file

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,11 +4,9 @@ import { Route } from 'react-router-dom';
 import PdfViewer from './PdfViewer';
 import Home from './Home';
 
-import styles from './App.css';
-
 export default function App() {
   return (
-    <section className={styles.root}>
+    <section>
       <Route exact path="/" component={Home} />
       <Route path="/cv" component={PdfViewer} />
     </section>
