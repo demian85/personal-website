26435e5f1b255a27bf37e3cc3244dbe661ff6984
Demian Rodriguez <demian85@gmail.com>
2019-03-10 14:29:57 -0300

    Update link

--- client/components/PdfViewer.jsx
+++ client/components/PdfViewer.jsx
@@ -6,12 +6,12 @@ export default function PdfViewer() {
   return (
     <object
       className={styles.pdf}
-      data="https://www.dropbox.com/s/6lwo3qk6bnksfyy/Demian_Andres_Rodriguez_CV_NEW.pdf?raw=1"
+      data="https://www.dropbox.com/s/6lwo3qk6bnksfyy/Demian_Andres_Rodriguez_CV.pdf?raw=1"
       type="application/pdf"
     >
       <p>
         This browser does not support inline PDFs.
-        <a href="https://www.dropbox.com/s/6lwo3qk6bnksfyy/Demian_Andres_Rodriguez_CV_NEW.pdf?raw=1">Please download the PDF</a> to view it.
+        <a href="https://www.dropbox.com/s/6lwo3qk6bnksfyy/Demian_Andres_Rodriguez_CV.pdf?raw=1">Please download the PDF</a> to view it.
       </p>
     </object>
   );
